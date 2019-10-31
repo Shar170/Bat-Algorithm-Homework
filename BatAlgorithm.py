49d14867660e3b4d8df7db4ff2babd1e55f9c104
Shar170 <Shar170@yandex.ru>
2019-10-31 06:32:25 +0300

    Реализация управления алгоритмом
    Корректное название переменных, Реализация функции Изома
    Запуск оптимизации в главном файле модуля

--- BatAlgorithm.py
+++ BatAlgorithm.py
@@ -3,8 +3,9 @@ import math
 
 np.random.seed(100)
 
+
 class BatAlgorithm():
-    def __init__(self, dimensi, n_bat, n_generasi, r0, alpha, gamma, fmin, fmax, b_bawah, b_atas, fungsi):
+    def __init__(self, dimensi, n_bat, n_generasi, r0, alpha, gamma, fmin, fmax, b_down, b_up, fungsi):
         self.dimensi = dimensi
         self.n_bat = n_bat
         self.n_generasi = n_generasi
@@ -12,132 +13,131 @@ class BatAlgorithm():
         self.gamma = gamma
         self.fmin = fmin
         self.fmax = fmax
-        self.b_bawah = b_bawah
-        self.b_atas = b_atas
+        self.b_down = b_down
+        self.b_up = b_up
         self.fungsi = fungsi
         self.epsilon = 0.001
         self.r0 = r0
-        
-        #inisialisasi nilai loudness (A) dan pulse rate (r)
+
+        # инициализировать значения громкости (A) и частоты пульса (r)
         self.A = [0.95 for i in range(self.n_bat)]
         self.r = [self.r0 for i in range(self.n_bat)]
-        
-        #inisialisasi upperbound dan lowerbound setiap bat
+
+        # инициализация верхней границы и нижней границы для каждой летучей мыши
         self.upbound = [[0.0 for i in range(self.dimensi)] for j in range(self.n_bat)]
-        self.lowbound =  [[0.0 for i in range(self.dimensi)] for j in range(self.n_bat)]
-        
-        #inisialisasi nilai 0 untuk semua bat
+        self.lowbound = [[0.0 for i in range(self.dimensi)] for j in range(self.n_bat)]
+
+        # инициализировать значение 0 для всех летучих мышей
         self.frekuensi = [0.0] * n_bat
-        
-        #inisialisasi nilai v (velocity) untuk semua bat
+
+        # инициализировать значение v (скорость) для всех летучих мышей
         self.v = [[0.0 for i in range(self.dimensi)] for j in range(self.n_bat)]
-        
-        #inisialisasi nilai x (lokasi/solusi) untuk semua bat
+
+        # инициализировать значения х (местоположение / решение) для всех летучих мышей
         self.x = [[0.0 for i in range(self.dimensi)] for j in range(self.n_bat)]
-        
-        #inisialisasi nilai fitness untuk semua bat
+
+        # инициализировать значение пригодности для всех летучих мышей
         self.nilai_fitness = [0.0] * n_bat
         self.nilai_fitness_minimum = 0.0
-        
-        #inisialisasi solusi terbaik
-        self.terbaik = [0.0] * dimensi
-    
+
+        # инициализация лучшего решения
+        self.best = [0.0] * dimensi
+
     def bat_terbaik(self):
         i = 0
         j = 0
-        #cari nilai fitness terbaik dan catat indeksnya pada variabel j
+        # найдите лучшее значение приспособленности и отметьте индекс по переменной j
         for i in range(self.n_bat):
-            if self.nilai_fitness[i] < self.nilai_fitness[j] :
+            if self.nilai_fitness[i] < self.nilai_fitness[j]:
                 j = i
-                
-        #simpan nilai-nilai dari setiap dimensi pada solusi terbaik
+
+        # сохранить значения каждого измерения в лучшем решении
         for i in range(self.dimensi):
-            self.terbaik[i] = self.x[j][i]
-        
-        #simpan nilai fitness dari solusi terbaik
+            self.best[i] = self.x[j][i]
+
+        # сохранить ценность приспособленности от лучшего решения
         self.nilai_fitness_minimum = self.nilai_fitness[j]
-    
+
     def proses_init(self):
-        #set semua upperbound dan lowerbound dengan parameter yang telah diset sebelumnya
+        # установить все верхние и нижние параметры, которые были установлены ранее
         for i in range(self.n_bat):
             for j in range(self.dimensi):
-                self.lowbound[i][j] = self.b_bawah
-                self.upbound[i][j] = self.b_atas
-        
-        #generate solusi baru dari lowerbound dan upperbound serta set frekuensi semua bat ke 0
-        #bat belum mencari target v = 0.
+                self.lowbound[i][j] = self.b_down
+                self.upbound[i][j] = self.b_up
+
+        # генерировать новые решения из нижнего и верхнего пределов и установить все частоты летучих мышей на 0
+        # bat не ищет цель v = 0.
         for i in range(self.n_bat):
             self.frekuensi[i] = 0
             for j in range(self.dimensi):
-                random = np.random.uniform(0,1)
+                random = np.random.uniform(0, 1)
                 self.v[i][j] = 0.0
-                self.x[i][j] = self.lowbound[i][j] + (self.upbound[i][j] - self.lowbound[i][j])*random
+                self.x[i][j] = self.lowbound[i][j] + (self.upbound[i][j] - self.lowbound[i][j]) * random
             self.nilai_fitness[i] = self.fungsi(self.x[i])
-        
-        #cari bat dengan nilai fitness terendah (minimum)
+
+        # найти летучую мышь с наименьшим значением пригодности (минимум)
         self.bat_terbaik()
-    
+
     def normalisasi_batas(self, nilai):
-        #jika nilai melebihi batas atas maka set nilai menjadi batas atas
-        if(nilai > self.b_atas):
-            nilai = self.b_atas
-            
-        #jika nilai lebih kecil dari batas bawah maka set nilai menjadi batas bawah
-        if(nilai < self.b_bawah):
-            nilai = self.b_bawah
-        
+        # если значение превышает верхний предел, то установленное значение становится верхним пределом
+        if (nilai > self.b_up):
+            nilai = self.b_up
+
+        # если значение меньше нижнего предела, установите значение нижнего пределаh
+        if (nilai < self.b_down):
+            nilai = self.b_down
+
         return nilai
-    
+
     def proses_ba(self):
-        #matriks solusi (banyak bat x dimensi)
+        # матричное решение (много размеров ЛМ х РАЗМЕРНОСТЬ)
         solusi = [[0.0 for i in range(self.dimensi)] for j in range(self.n_bat)]
-        
+
         self.proses_init()
         print(self.nilai_fitness)
-        
+
         for n in range(self.n_generasi):
             Arata2 = np.mean(self.A)
             for i in range(self.n_bat):
-                random = np.random.uniform(0,1)
-                #cari frekuensi setiap bat dengan menggunakan eq. 2 dari bat algorithm
-                self.frekuensi[i] = self.fmin + (self.fmax-self.fmin)*random
+                random = np.random.uniform(0, 1)
+                # найти частоту каждой летучей мыши с помощью уравнения 2
+                self.frekuensi[i] = self.fmin + (self.fmax - self.fmin) * random
                 for j in range(self.dimensi):
-                    #cari v dan x baru dari setiap bat dengan menggunakan eq. 3 dan 4 dari bat algorithm
-                    self.v[i][j] = self.v[i][j] + (self.x[i][j] - self.terbaik[j])*self.frekuensi[i]
+                    # найти новые v и x каждой летучей мыши, используя уравнение 3 и 4 алгоритма летучей мыши
+                    self.v[i][j] = self.v[i][j] + (self.x[i][j] - self.best[j]) * self.frekuensi[i]
                     solusi[i][j] = self.x[i][j] + self.v[i][j]
                     solusi[i][j] = self.normalisasi_batas(solusi[i][j])
-                
-                random = np.random.uniform(0,1)
-                #jika nilai random [0,1] lebih besar dari nilai pulse rate dari bat tersebut, maka lakukan local search berdasarkan bat terbaik
-                if(random > self.r[i]):
+
+                random = np.random.uniform(0, 1)
+                # если случайное значение [0,1] больше значения частоты пульса летучей мыши, то выполнить локальный
+                # поиск на основе лучшей летучей мыши
+                if (random > self.r[i]):
                     for j in range(self.dimensi):
-                        random = np.random.uniform(-1.0,1.0)
-                        solusi[i][j] = self.terbaik[j] + random*Arata2
+                        random = np.random.uniform(-1.0, 1.0)
+                        solusi[i][j] = self.best[j] + random * Arata2
                         solusi[i][j] = self.normalisasi_batas(solusi[i][j])
-                
-                
-                #hitung nilai fitness dari solusi baru
+
+                # рассчитать пригодность нового решения
                 nilai_fitness = self.fungsi(solusi[i])
-                
-                random = np.random.uniform(0,1)
-                
-                if(random < self.A[i] and nilai_fitness < self.nilai_fitness[i]):
+
+                random = np.random.uniform(0, 1)
+
+                if (random < self.A[i] and nilai_fitness < self.nilai_fitness[i]):
                     self.nilai_fitness[i] = nilai_fitness
                     for j in range(self.dimensi):
                         self.x[i][j] = solusi[i][j]
-                
-                if(self.nilai_fitness[i] < self.nilai_fitness_minimum):
-                    #ganti solusi terbaik
+
+                if (self.nilai_fitness[i] < self.nilai_fitness_minimum):
+                    # заменить лучшее решение
                     self.nilai_fitness_minimum = self.fungsi(solusi[i])
                     for j in range(self.dimensi):
-                        self.terbaik[j] = self.x[i][j] 
-                    
-                    #update nilai loudness dan pulse rate setiap bat
-                    self.A[i] = self.A[i]*self.alpha
-                    self.r[i] = self.r0*(1 - math.exp(-1*self.gamma*i))
-            print("Nilai fitness generasi ke (",n,") : ",self.nilai_fitness)
-            print("Nilai fitness terbaik ",self.nilai_fitness_minimum)
-            print("Solusi terbaik ",self.terbaik)
+                        self.best[j] = self.x[i][j]
+
+                        # обновлять громкость и частоту пульса каждой летучей мыши
+                    self.A[i] = self.A[i] * self.alpha
+                    self.r[i] = self.r0 * (1 - math.exp(-1 * self.gamma * i))
+            print("Ценность поколения для расчёта (", n, ") : ", self.nilai_fitness)
+            print("Лучшая ценность для расчёта ", self.nilai_fitness_minimum)
+            print("Лучшее решение ", self.best)
         print(self.nilai_fitness_minimum)
-        print(self.terbaik)
-        
+        print(self.best)
--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import random
+import math
+import numpy as np
 from BatAlgorithm import *
 
 
-def fungsi(d):
-    jum = 0
-    for i in range(len(d)):
-        jum = jum + (d[i]*d[i])
-    return jum
+def funEasom(d):
+    res = 1
+    for i in d:
+        res = res * math.cos(i) * math.exp(-1.0 * np.sum((np.array(d) - math.pi * np.ones(np.array(d).shape)) ** 2))
+    if len(d) % 2 == 0:
+        res = -1 * res
+    return res
 
-ba = BatAlgorithm(2,1000,10000,0.1,0.95,0.95,0,1,-1,1,fungsi)
+
+dimensi= 2
+n_bat=100
+n_generasi=1000
+r0=0.1
+alpha=0.95
+gamma=0.95
+fmin=0
+fmax=1
+b_down =-10
+b_up =10
+
+ba = BatAlgorithm(dimensi, n_bat, n_generasi, r0, alpha, gamma, fmin, fmax, b_down, b_up, funEasom)
+
 ba.proses_ba()
-
